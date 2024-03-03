9e5c1a58b8e22e9c81fed2b8178d6c7e36e9c8fc
LoperaUr <manuellopera612@gmail.com>
2024-03-03 11:42:45 -0500

    Añadir filtrado por género y búsqueda por ID de película en las rutas de la API

--- app.js
+++ app.js
@@ -7,7 +7,19 @@ const movies = require('./movies.json')
 app.disable('x-powered-by')
 
 app.get('/movies', (req, res) => {
+  const { genre } = req.query
+  if (genre) {
+    const filteredMovies = movies.filter((movie) => movie.genre.some((g) => g.toLowerCase() === genre.toLowerCase()))
+    return res.json(filteredMovies)
+  }
   res.json(movies)
+})
+
+app.get('/movies/:id', (req, res) => {
+  const { id } = req.params
+  const movie = movies.find((movie) => movie.id === id)
+  if (movie) return res.json(movie)
+  res.status(404).json({ error: 'Movie not found' })
 })
 
 app.listen(PORT, () => {
