fa1a0ce00d2e8b75fd0f16d2f72664a027617320
LoperaUr <--global>
2024-03-12 22:59:34 -0500

    Actualizar esquema de películas a zod

--- schemas/movies.js
+++ schemas/movies.js
@@ -1,19 +1,19 @@
-import z from 'zod'
+import zod from 'zod'
 
-const movieSchema = z.object({
-  title: z.string({
+const movieSchema = zod.object({
+  title: zod.string({
     invalid_type_error: 'Movie title must be a string',
     required_error: 'Movie title is required.'
   }),
-  year: z.number().int().min(1900).max(2024),
-  director: z.string(),
-  duration: z.number().int().positive(),
-  rate: z.number().min(0).max(10).default(5),
-  poster: z.string().url({
+  year: zod.number().int().min(1900).max(2024),
+  director: zod.string(),
+  duration: zod.number().int().positive(),
+  rate: zod.number().min(0).max(10).default(5),
+  poster: zod.string().url({
     message: 'Poster must be a valid URL'
   }),
-  genre: z.array(
-    z.enum(['Action', 'Adventure', 'Crime', 'Comedy', 'Drama', 'Fantasy', 'Horror', 'Thriller', 'Sci-Fi']),
+  genre: zod.array(
+    zod.enum(['Action', 'Adventure', 'Crime', 'Comedy', 'Drama', 'Fantasy', 'Horror', 'Thriller', 'Sci-Fi']),
     {
       required_error: 'Movie genre is required.',
       invalid_type_error: 'Movie genre must be an array of enum Genre'
